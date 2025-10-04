a36dbe7db67fd62217367fb969d627702170185d
Pierre Schranz <schranz67@yahoo.fr>
2025-10-04 19:01:11 +0200

    feat:Firsts routes

---
+++ src/Controller/EventController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class EventController extends AbstractController
+{
+    #[Route('/event')]
+    public function index(): Response
+    {
+        return $this->render('event/index.html.twig');
+    }
+}
