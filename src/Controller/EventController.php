ed5d77a0a0d9448b9aa07bd0f24790c74e34fffd
Pierre Schranz <schranz67@yahoo.fr>
2025-10-06 20:33:07 +0200

    feat: Header&Footer

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -7,12 +7,37 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Twig\Template;
 
 class EventController extends AbstractController
 {
-    #[Route('/event')]
-    public function index(): Response
+    /**
+     * Page liste
+     * @return Template
+     */
+    #[Route('/list', name: 'events_list', methods: ['GET'])]
+    public function list()
     {
-        return $this->render('event/index.html.twig');
+        return $this->render('events/list.html.twig');
+    }
+
+    /**
+     * Page detail
+     * @return Template
+     */
+    #[Route('/detail', name: 'events_detail', methods: ['GET'])]
+    public function detail()
+    {
+        return $this->render('events/detail.html.twig', ['id' => 1]);
+    }
+
+    /**
+     * Page detail
+     * @return Template
+     */
+    #[Route('/detail/{id}', name: 'events_detail_id', methods: ['GET'])]
+    public function detail_id($id)
+    {
+        return $this->render('events/detail.html.twig', ['id' => $id]);
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,8 +3,38 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+use Twig\Template;
 
 class UserController extends AbstractController
 {
+    /**
+     * Page rôle
+     * @return Template
+     */
+    #[Route('/role', name: 'users_role', methods: ['GET'])]
+    public function role()
+    {
+        return $this->render('users/handle_role.html.twig');
+    }
 
+    /**
+     * Page rôle
+     * @return Template
+     */
+    #[Route('/account', name: 'users_create_account', methods: ['GET'])]
+    public function create_account()
+    {
+        return $this->render('users/create_account.html.twig');
+    }
+
+    /**
+     * Page rôle
+     * @return Template
+     */
+    #[Route('/login', name: 'users_login', methods: ['GET'])]
+    public function login()
+    {
+        return $this->render('users/login.html.twig');
+    }
 }
